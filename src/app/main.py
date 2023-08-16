10080dd415f67f58e7ef5ad2a239fbffa4ad4cb7
joae1234 <joaodoscubos@gmail.com>
2023-08-16 18:40:04 -0300

    a

--- src/app/main.py
+++ src/app/main.py
@@ -18,6 +18,7 @@ repo = Environments.get_item_repo()()
 
 @app.get("/items/get_all_items")
 def get_all_items():
+    print("Entrando no get all itens")
     clientes = repo.get_all_items()
     clientes_list = list()
     for cliente in clientes:
