84c34ba3ece2972d13777d7f419546fa6afcc583
joae1234 <joaodoscubos@gmail.com>
2023-08-19 14:38:14 -0300

    corrigindo transaçoes com erro

--- src/app/main.py
+++ src/app/main.py
@@ -47,11 +47,13 @@ def deposit(request: dict):
     timestamp = int(round(time.time() * 1000))
     
     addition=notes_2*2.0 + notes_5*5.0+notes_10*10.0+notes_20*20.0+notes_50*50.0+notes_100*100.0+notes_200*200.0
-    res=newBalance+addition
-    transactions.append({"type": "deposit",     "value": addition,    "current_balance": res, "timestamp": timestamp})
-    #repo.update_balance(0,res)
+
+     #repo.update_balance(0,res)
     if newBalance*2<=addition:
         raise HTTPException(status_code=403, detail="Depósito suspeito")
+    res=newBalance+addition
+    transactions.append({"type": "deposit",     "value": addition,    "current_balance": res, "timestamp": timestamp})
+   
     return {
         "current_balance": res,
         "timestamp": timestamp 
@@ -85,11 +87,13 @@ def deposit(request: dict):
     timestamp = int(round(time.time() * 1000))
     
     addition=notes_2*2.0 + notes_5*5.0+notes_10*10.0+notes_20*20.0+notes_50*50.0+notes_100*100.0+notes_200*200.0
-    res=newBalance-addition
-    transactions.append({"type": "withdraw",     "value": addition,    "current_balance": res, "timestamp": timestamp})
+    
     #repo.update_balance(0,res)
     if newBalance<addition:
         raise HTTPException(status_code=403, detail="Saldo insuficiente para transação.")
+    res=newBalance-addition
+    transactions.append({"type": "withdraw",     "value": addition,    "current_balance": res, "timestamp": timestamp})
+   
     return {
         "current_balance": res,
         "timestamp": timestamp 
